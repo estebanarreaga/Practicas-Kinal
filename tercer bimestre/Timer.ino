69b5b3fb6b2ef2550dcc697a7e61e87f159d34f9
estebanarreaga <127925763+estebanarreaga@users.noreply.github.com>
2023-06-08 08:30:06 -0600

    Add files via upload

---
+++ tercer bimestre/Timer.ino
@@ -0,0 +1,25 @@
+/*
+ * Fundacion Kinal
+ * Centro educativo tecnico laboral Kinal
+ * Electronica
+ * Grado: Quinto
+ * Sección: A
+ * Curso: Taller de electronica digital y reparación de computadoras I
+ * Nombre: Esteban Andres Rivas Arreaga
+ * Carnet: 2020198
+ * Proyecto: Contador de 0-50
+*/
+
+#define ledsito 3
+#define potenciometrito A0
+int intencidadsita;
+
+void setup() {
+  pinMode(ledsito, OUTPUT);
+}
+ 
+
+void loop() {
+ intencidadsita= analogRead(potenciometrito)/4;
+ analogWrite(ledsito, intencidadsita);
+}
